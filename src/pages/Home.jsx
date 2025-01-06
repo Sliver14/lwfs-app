6a580c2a353c177736dcaae04858bc94904fe2e2
sliver14 <silverchristopher12@gmail.com>
2025-01-06 19:54:42 +0100

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,7 @@ function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [isLogIn, setIsLogIn] = useState(true);
   const [isRegister, setIRegister] = useState(false);
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [loggedIn, setLoggedIn] = useState(true);
 
   useEffect(() => {
     if(!loggedIn){
