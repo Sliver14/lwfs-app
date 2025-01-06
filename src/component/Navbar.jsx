36c47dea0ffeca1cdf287a98385fc460b91957d8
sliver14 <silverchristopher12@gmail.com>
2025-01-06 15:09:16 +0100

    update

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 // import "../style/Navbar.css";
 import { useState, useEffect } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import { useNavigate, Link, NavLink } from 'react-router-dom';
 import { GrMenu } from "react-icons/gr";
 import { IoMdClose } from "react-icons/io";
 import { MdOutlineEmail } from "react-icons/md";
 import { FiPhoneCall } from "react-icons/fi";
+import { LiaHomeSolid } from "react-icons/lia";
+import { MdLiveTv } from "react-icons/md";
+import { GiPlatform } from "react-icons/gi";
+
 
 
 import logo from "../images/LWFS_LOGO.png";
@@ -23,55 +27,33 @@ function Navbar() {
     <div className='flex flex-col '>
       {/* Navbar */}
     <div className='flex flex-col w-screen bg-white fixed z-10 ' >
-      
-      <div className='flex flex-col bg-lwfs1 text-white p-3 gap-2' >
-      <div className='flex gap-2 cursor-pointer'><MdOutlineEmail /><h5 className='text-xs font-bold'>info@lwfoundationschool.com</h5></div>
-
-      <div className='flex gap-2 cursor-pointer'><FiPhoneCall /><h5 className='text-xs font-bold'>+234 80 3502 4986</h5></div>
-
-      <div className='flex cursor-pointer gap-2' ><img className='w-5 h-5' src='./images/kingschat.png' alt='image'/><a className='text-xs font-bold' href='https://kingschat.online/user/lwfsch' target="_blank">Follow Us</a></div>
-      </div>
     
       <div className='relative flex flex-row justify-center items-center py-1 w-screen'>
-        {/* Toggle */}
-        <button className="absolute left-0 cursor-pointer p-4" onClick={toggleSidebar}>
-          <GrMenu/>
-        </button>
 
         <img onClick={() => {navigate("/")}} className='h-10 cursor-pointer' src={logo} alt="lwfs_logo"/>
         
       </div>
     </div>
 
-    {/* Opened Sidebar */}
-    <div className={`fixed  top-0 left-0 justify-center w-screen h-full bg-white  overflow-hidden transition ease-in-out duration-300 z-20  ${isOpen ? "-translate-x-12" : "-translate-x-full"}`}>
-    <div className='flex flex-col w-screen ml-9'>
+    {/* botoom Navbar */}
+    <div className='fixed flex bottom-0 right-0 left-0 justify-center items-center p-3 bg-white shadow-lg z-20'>
+      <div>
+        <div className='flex gap-3'>
+          <NavLink to="/" className={({isActive}) => `flex items-center  ${isActive ? "text-blue-500" : "text-black"}`}>
+          <LiaHomeSolid className='text-3xl'/>
+          </NavLink>
 
-    <div className='flex flex-row w-full' onClick={toggleSidebar} >
-                    
-        {/* Toggle */}
-        <button className="ml-3 p-5" ><IoMdClose />
-      </button>
-    </div>
-
-      <div className='flex flex-col h-screen' >
-        <ul className='px-5 space-y-2 font-bold' >
-          <li className=' p-2 cursor-pointer hover:bg-slate-900 hover:text-white ' onClick={() => {toggleSidebar(); navigate("/")}}>
-            HOME
-          </li>
-          <li className='p-2 cursor-pointer hover:bg-slate-900 hover:text-white ' onClick={() => {toggleSidebar(); navigate("/live-tv")}}>
-          LIVE-TV
-          </li>
-        
-          <li className='p-2 cursor-pointer hover:bg-slate-900 hover:text-white' >LWFS Store</li>
-          <li className='p-2 cursor-pointer hover:bg-slate-900 hover:text-white' >Online foundation School/Exams</li>
-          <li className='p-2 cursor-pointer hover:bg-slate-900 hover:text-white' >Testimony Bank</li>
-        </ul>
-      </div>
-    </div>
-      
+          <NavLink to="/live-tv" className={({isActive}) => `flex items-center  ${isActive ? "text-blue-500" : "text-black"}`}>
+          <MdLiveTv className='text-3xl'/>
+          </NavLink>
+          <NavLink to="/platforms" className={({isActive}) => `flex items-center  ${isActive ? "text-blue-500" : "text-black"}`}>
+          <GiPlatform className='text-3xl'/>
+          </NavLink>
+        </div>
         
       </div>
+    </div>
+
   
 </div>
 
---
+++ src/pages/Platforms.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Platforms() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Platforms
